c7863b695ddafd0c4ab28a87b9a9352267744699
hoanganhchung92015-hub <hoanganhchung92015@gmail.com>
2025-12-19 03:07:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
+  // Đảm bảo không còn dòng nào chứa process.env ở đây
 })
